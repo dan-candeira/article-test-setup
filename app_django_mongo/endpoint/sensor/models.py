293a44c9905a5679985f4a172133dec66af4f887
dan-candeira <danielbarr16@gmail.com>
2021-08-25 21:21:18 -0300

    fix: reestructure project setup

--- app_django_mongo/endpoint/sensor/models.py
+++
@@ -1,8 +0,0 @@
-from djongo import models
-
-
-class Sensor(models.Model):
-    _id = models.ObjectIdField(primary_key=True)
-    measuring_dimentions = models.JSONField(null=False)
-    model = models.CharField(max_length=255)
-    description = models.TextField(null=True)
